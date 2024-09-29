d7595287e7cff2ed8cea3ab72c9ccc6b35ccea56
sphynx-code <proanimelover26@gmail.com>
2024-09-30 00:17:35 +0500

    added vercel json file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to serve static files
 app.use(express.static("public"));
